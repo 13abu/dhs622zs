07012db58b4b6694f768a49a7ba515e0e523348a
Zeerak's Laptop <zee00rak@gmail.com>
2026-03-24 04:12:11 +0300

    week 10 added

---
+++ week10/run_channel_info_retriever_no_rabbit.py
@@ -0,0 +1,22 @@
+## Proof of concept with config data hidden and data dictionary flattened
+
+import asyncio
+import sys
+
+# Create and set event loop before any Telethon imports
+if sys.platform in ('win32', 'darwin'):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+from week10.utilities.logic import retrieve_and_save_channel_metadata
+from week10.config import app_name, api_id, api_hash, INPUT_DIR
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+    seed_list_name = 'indian_military_nationalism'
+    channel_names = ["IndianMilitaryUpdates", "bnpkukrti", "BadrulBSL","Diplomatic_Talk","Turkeypedia","newsIEA","OsintTV","russiaukrwar","vishwahinduektasangh","hindu"]
+
+    retrieve_and_save_channel_metadata(
+        channel_names, app_name, api_id, api_hash, seed_list_name
+    )
--- week10/week10/config.py
+++ week10/week10/config.py
@@ -18,3 +18,7 @@ config.read(config_file_full_path)
 app_name = config["telegram-credentials-1"]["app-name"]
 api_id = config["telegram-credentials-1"]["api-id"]
 api_hash = config["telegram-credentials-1"]["api-hash"]
+
+api_host = "127.0.0.1"
+api_port = 8000
+api_base = f"http://{api_host}:{api_port}"
